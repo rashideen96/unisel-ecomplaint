1be8a8f89e5cdfdca7eaf2192ebb142cf734909f
vertexPlayer <xyberworm@yahoo.com>
2020-02-17 17:33:08 +0800

    update sql

--- sql/ecomplaint.sql
+++ sql/ecomplaint.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2019 at 05:29 PM
+-- Generation Time: Feb 17, 2020 at 10:32 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 5.6.34
 
@@ -46,6 +46,39 @@ INSERT INTO `admin` (`id`, `role`, `admin_id`, `email`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `complaints`
+--
+
+CREATE TABLE `complaints` (
+  `id` int(11) NOT NULL,
+  `complainer_id` int(11) NOT NULL,
+  `technician_id` int(11) NOT NULL DEFAULT '0',
+  `status_id` int(11) NOT NULL DEFAULT '0',
+  `room_no` varchar(255) NOT NULL,
+  `complaint_type` int(11) NOT NULL,
+  `img1` varchar(255) NOT NULL,
+  `img2` varchar(255) NOT NULL,
+  `img3` varchar(255) NOT NULL,
+  `detail` text NOT NULL,
+  `available_date` varchar(50) NOT NULL,
+  `available_time` varchar(255) NOT NULL,
+  `comment` text,
+  `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `complaints`
+--
+
+INSERT INTO `complaints` (`id`, `complainer_id`, `technician_id`, `status_id`, `room_no`, `complaint_type`, `img1`, `img2`, `img3`, `detail`, `available_date`, `available_time`, `comment`, `date_added`, `date_modified`) VALUES
+(2, 6, 6, 2, 'A3-2F-U4', 1, 'download.jpg', 'images.png', 'dropbox.jpg', '<p>rosak...</p>', '31/01/2020', '9:30am-12:00pm', 'Selesai', '2020-01-31 22:32:12', '2020-02-16 23:28:22'),
+(3, 6, 1, 1, 'A4-GF-U2', 2, 'download.jpg', 'images.png', 'images.jpg', '<p>Wifi selalu terputus</p>', '01/02/2020', '9:00am-10:30am', 'kiv', '2020-01-31 22:45:30', '2020-02-15 19:00:22'),
+(4, 6, 1, 0, 'B3-2F-U4', 2, 'WIN_20180205_112212.JPG', 'WIN_20180205_112230.JPG', 'WIN_20180205_112230.JPG', 'Tiada internet ', '15/02/2020', '9:00am-4:00pm', 'selesai', '2020-02-15 01:30:21', '2020-02-15 18:32:55');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `complaint_tbl`
 --
 
@@ -74,20 +107,32 @@ INSERT INTO `complaint_tbl` (`id`, `complainer_id`, `technician_id`, `no_bilik`,
 (25, 8, 1, 'B2-4F-U2', 'wifi', 'wifi selalu terputus', '28/05/2019', '9:30am - 11:00am', 'Closed'),
 (26, 1, 2, 'C3-3F-U4', 'furniture', 'perabot rosak', '26/05/2019', '9:00am - 12:30pm', 'Pending'),
 (27, 8, 1, 'A3-2F-U2', 'wifi', 'Wifi sentiasa terputus....', '31/05/2019', '10:00am - 1:30pm', 'KIV'),
-(28, 8, 1, 'A3-F3-U3', 'electrical', '', '12/05/2019', '10:30am - 11:00am', 'KIV');
+(28, 8, 1, 'A3-F3-U3', 'electrical', '', '12/05/2019', '10:30am - 11:00am', 'KIV'),
+(29, 6, 0, 'A2-2F-U3', 'electrical', 'lampu rosak', '30/12/2019', '9:00am - 10:30am', NULL),
+(30, 6, 0, '42-34-32-4', 'electrical', '3333', '31/12/2019', '9:00am - 11:00am', NULL),
+(31, 6, 0, '45-54-54-33-4', 'furniture', 'sasasasasas', '29/12/2019', '10:30am - 4:30pm', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contact`
+-- Table structure for table `complaint_type`
 --
 
-CREATE TABLE `contact` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `email` varchar(250) DEFAULT NULL,
-  `comment` varchar(250) DEFAULT NULL,
-  `createdDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE `complaint_type` (
+  `id` int(11) NOT NULL,
+  `type` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `complaint_type`
+--
+
+INSERT INTO `complaint_type` (`id`, `type`) VALUES
+(1, 'Electrical/Peralatan Elektrik'),
+(2, 'Modem/Wifi'),
+(3, 'Furniture/Perabot'),
+(4, 'Civil/Sivil'),
+(5, 'Security/Keselamatan');
 
 -- --------------------------------------------------------
 
@@ -178,7 +223,8 @@ INSERT INTO `message_tbl` (`id`, `complaint_id`, `mesej`) VALUES
 (78, 26, 'mesej dari technician'),
 (79, 27, 'mesej dari admin'),
 (80, 27, 'farah hensem'),
-(81, 27, 'admin prob');
+(81, 27, 'admin prob'),
+(82, 26, 'test');
 
 -- --------------------------------------------------------
 
@@ -202,8 +248,10 @@ CREATE TABLE `technician` (
 --
 
 INSERT INTO `technician` (`id`, `role`, `staff_id`, `department`, `name`, `phone_number`, `email`, `password`) VALUES
-(1, 'technician', '4444', 'cict', 'cict', '0122222222', 'cict@unisel.edu.my', '123'),
-(2, 'technician', '5555', 'ausa', 'ausa', '3445432424', 'ausa@gmail.com', '123');
+(1, 'technician', '4444', 'CICT', 'CICT', '0122222222', 'cict@unisel.edu.my', '123'),
+(4, 'technician', 'qoqle', 'JPP', 'JPP', '11223344556677', 'jpp@unisel.edu.my', 'jpp123'),
+(6, 'technician', 'scw9e', 'Asset Management', 'Asset Management', '4444444444', 'asset_management@unisel.edu.my', '123'),
+(7, 'technician', 'jb2g0', 'RMU', 'RMU', '3333333333', 'rmu@unisel.edu.my', '123');
 
 -- --------------------------------------------------------
 
@@ -272,10 +320,12 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`userId`, `role`, `name`, `username`, `email`, `password`, `icNum`, `matricNum`, `phoneNum`, `faculty`, `location`, `programe`, `staffPosition`, `CreatedDate`) VALUES
 (1, 'student', 'hafiz che hamid', 'hafiz', 'hafiz@gmail.com', '1', '960407105823', '4164004561', '0133634357', 'fab', 'sa', 'information technology', '', '2018-11-08 08:29:16'),
 (2, 'student', 'mirul', 'et', 'et@123', '123', '33', '22', '22', 'fcvac', 'bj', 'it cs', '', '2018-11-08 08:29:27'),
-(4, 'staff', 'hafizuddin', 'hafizzuddin', 'hafizzuddin@gmail.com', '123', NULL, '33343', '0122009642', 'fels', 'bj', NULL, 'lecturer', '2018-11-08 08:29:03'),
-(5, 'admin', 'admin', 'admin', 'admin@gmail.com', '123', '34434', '3164004561', '0122236014', 'cvac', 'sa', 'it', 'staff', '2019-05-22 20:55:02'),
-(6, 'student', 'mirul', 'mirul', 'hafiz@gmail.com', '123', '32223334', '3164004561', '0122236014', 'Faculty Communication Visual Arts & Computing', 'BJ', 'DIT', '', '2019-05-26 10:35:08'),
-(8, 'student', 'mirul', 'mirul', 'mirul@gmail.com', '123', '32223334', '3164004561', '0133634357', 'Faculty Communication Visual Arts & Computing', 'BJ', 'it', '', '2019-05-26 12:25:09');
+(4, 'staff', 'hafizuddin', 'hafizzuddin', 'hafizzuddin@gmail.com', '$2y$10$orWFRd.JmdZTPAwKmrDuNOYY3FC4BaFiHzVVml9ibtmofpbnOXuEm', NULL, '33343', '0122009642', 'fels', 'bj', NULL, 'lecturer', '2020-01-17 11:25:37'),
+(6, 'student', 'mirul', 'miruldeen', 'hafiz@gmail.com', '$2y$10$orWFRd.JmdZTPAwKmrDuNOYY3FC4BaFiHzVVml9ibtmofpbnOXuEm', '32223334', '3164004561', '0122236788', 'Faculty Communication Visual Arts & Computing', 'BJ', 'Diploma in Information Technology', '', '2019-12-30 03:15:27'),
+(9, 'student', 'dsadasdas', 'dsadasdas', 'sadbask@1122', '$2y$10$mQegMyAaaodTokfb5t/NueP0N2QFPrw2j7KvceRAhKyD3tqzcqCR.', NULL, '32443444', '342453254566', 'Faculty Communication Visual Arts & Computing', NULL, 'sdadsadas', '', '2019-12-27 16:57:07'),
+(10, 'student', 'dsadasdas', 'dsadasdas', 'sadbask@1122', '$2y$10$BHNKpUSVJiHrxJ9g.KdcYOx4Box7OhUNS2UcByP93ncNMSzpek.Au', NULL, '32443444', '342453254566', 'Faculty Communication Visual Arts & Computing', NULL, 'sdadsadas', '', '2019-12-27 17:04:52'),
+(11, 'student', 'dsadasdas', 'dsadasdas', 'sadbask@1122', '$2y$10$3uo..bGfY6bOsqtC9Ol5U.dI6T5OK3FiyvdowxnMgswxjLoq/n25G', NULL, '32443444', '342453254566', 'Faculty Education & Science Social', NULL, 'sdadsadas', '', '2019-12-27 17:07:22'),
+(12, 'student', 'dsadasdas', 'dsadasdas', 'sadbask@1122', '$2y$10$XkMlACoTCFQxoY9p9iu3Eef8FAaYqzxB8lc9HHNlOBMJjcZ.oHNuC', NULL, '32443444', '342453254566', 'Faculty Communication Visual Arts & Computing', NULL, 'sdadsadas', '', '2019-12-27 17:10:02');
 
 --
 -- Indexes for dumped tables
@@ -288,15 +338,21 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `complaints`
+--
+ALTER TABLE `complaints`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `complaint_tbl`
 --
 ALTER TABLE `complaint_tbl`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `contact`
+-- Indexes for table `complaint_type`
 --
-ALTER TABLE `contact`
+ALTER TABLE `complaint_type`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -352,16 +408,22 @@ ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `complaints`
+--
+ALTER TABLE `complaints`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `complaint_tbl`
 --
 ALTER TABLE `complaint_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
--- AUTO_INCREMENT for table `contact`
+-- AUTO_INCREMENT for table `complaint_type`
 --
-ALTER TABLE `contact`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `complaint_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `contractor`
@@ -385,13 +447,13 @@ ALTER TABLE `matric_tbl`
 -- AUTO_INCREMENT for table `message_tbl`
 --
 ALTER TABLE `message_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- AUTO_INCREMENT for table `technician`
 --
 ALTER TABLE `technician`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `upload_img`
@@ -403,7 +465,7 @@ ALTER TABLE `upload_img`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `userId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
